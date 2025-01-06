11dcd525e403c1204cd49ea7a577200ebed77824
ecper <utikasu@gmail.com>
2025-01-06 13:53:53 +0900

    メール送信 cc追加

--- src/libraries/Email.js
+++ src/libraries/Email.js
@@ -1,31 +1,33 @@
-import transporter from "~/config/emailConfig";
+import transporter from '~/config/emailConfig';
 import hbs from 'nodemailer-express-handlebars';
 
 class Email {
-    // trigger the sending of the E-mail
-    async sendEmail(mailOptions) {
-        mailOptions.from = process.env.MAIL_USERNAME;
-        // use a template file with nodemailer
-        transporter.use('compile', hbs(mailOptions))
+  // trigger the sending of the E-mail
+  async sendEmail(mailOptions) {
+    mailOptions.from = process.env.MAIL_USERNAME;
+    mailOptions.cc = [process.env.MAIL_CC];
+    // use a template file with nodemailer
+    transporter.use('compile', hbs(mailOptions));
 
-        // send mail 
-        return transporter.sendMail(mailOptions).then((response) => {
-            var res = {
-                status: true,
-                response: response
-            };
+    // send mail
+    return transporter
+      .sendMail(mailOptions)
+      .then((response) => {
+        var res = {
+          status: true,
+          response: response,
+        };
 
-            return res;
-
-        }).catch((err) => {
-            console.log(err);
-            var res = {
-                status: false,
-                response: err
-            };
-            return res;
-        })
-
-    }
+        return res;
+      })
+      .catch((err) => {
+        console.log(err);
+        var res = {
+          status: false,
+          response: err,
+        };
+        return res;
+      });
+  }
 }
 module.exports = Email;
